242caf6cf2aee456c21f454a7b46a3bedeffadf3
nelsonleopold <nelsonleopold@gmail.com>
2022-04-26 14:50:44 -0700

    added virtuals to ApplicationDbContext

--- GenesisAddressBook/Data/ApplicationDbContext.cs
+++ GenesisAddressBook/Data/ApplicationDbContext.cs
@@ -9,7 +9,9 @@ namespace GenesisAddressBook.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
         }
+
+        public virtual DbSet<Contact> Contacts { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!;
     }
 }
