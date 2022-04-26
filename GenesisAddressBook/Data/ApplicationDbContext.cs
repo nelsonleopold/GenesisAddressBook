f3d01931ab9244d5740a476714e0d00de4c8e440
nelsonleopold <nelsonleopold@gmail.com>
2022-04-26 14:10:03 -0700

    Add project files.

---
+++ GenesisAddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GenesisAddressBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenesisAddressBook.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
